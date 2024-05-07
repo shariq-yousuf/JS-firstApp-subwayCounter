ab848a30fd693cb104a248f86285350dda4d2032
shariq-yousuf <shariqyousuf01@gmail.com>
2024-05-07 19:55:44 +0500

    addToRemote

---
+++ script.js
@@ -0,0 +1,19 @@
+let count = 0;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let enteries =  count + " - ";
+    saveEl.textContent += enteries;
+    count = 0;
+    countEl.textContent = 0;
+}
+
+
+
+console.log()
